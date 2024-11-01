311cab44c7a60bf47f69b4cd4ccc3e8ea1c60ebd
DmitriyShkredov <dmitriyshkredovdev@gmail.com>
2024-11-01 08:46:29 +0300

    update func to add script dinamicaly

--- js/add_script_dinamicaly.js
+++ js/add_script_dinamicaly.js
@@ -1,6 +1,20 @@
+// ADD ANY SCRIPT
 function addScript( src, callback ) {
     const s = document.createElement( 'script' );
     s.setAttribute( 'src', src );
     s.onload=callback;
     document.body.appendChild( s );
 }
+
+// ADD TELEGRAM API
+const script = document.createElement( 'script' );
+script.setAttribute( 'src', "https://telegram.org/js/telegram-web-app.js" );
+script.onload = () => {
+    if(window && window.Telegram && window.Telegram.WebApp) {
+        const tg = window.Telegram.WebApp;
+        tg.expand();
+        setTimeout(() => { tg.expand() }, 500);
+        console.log("+++ Telegram ", tg.version);
+    }
+};
+document.body.appendChild(script);
