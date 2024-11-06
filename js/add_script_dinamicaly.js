ff8414b7828ba6acae9434516d076f7097d05c16
DmitriyShkredov <dmitriyshkredovdev@gmail.com>
2024-11-06 10:28:17 +0300

    update telegram script

--- js/add_script_dinamicaly.js
+++ js/add_script_dinamicaly.js
@@ -1,22 +1,24 @@
 // ADD ANY SCRIPT
 function addScript( src, callback ) {
-    const s = document.createElement( 'script' );
-    s.setAttribute( 'src', src );
-    s.onload=callback;
-    document.body.appendChild( s );
+  const s = document.createElement( 'script' );
+  s.setAttribute( 'src', src );
+  s.onload=callback;
+  document.body.appendChild( s );
 }
 
 // ADD TELEGRAM API
 const script = document.createElement( 'script' );
-script.setAttribute( 'src', "https://telegram.org/js/telegram-web-app.js" );
-script.onload = () => {
+script.onload = function() {
+  setTimeout(() => {
     if(window && window.Telegram && window.Telegram.WebApp) {
-        const tg = window.Telegram.WebApp;
-        tg.expand();
-        setTimeout(() => { tg.expand() }, 500);
-        tg.disableVerticalSwipes();
-        tg.allow_vertical_swipe = false;
-        console.log("TG VERSION", tg.version);
+      const tg = window.Telegram.WebApp;
+      tg.expand();
+      setTimeout(() => { tg.expand() }, 500);
+      tg.disableVerticalSwipes();
+      tg.allow_vertical_swipe = false;
+      console.log("TG VERSION", tg.version);
     }
+  }, 500)
 };
+script.setAttribute( 'src', "https://telegram.org/js/telegram-web-app.js" );
 document.body.appendChild(script);
